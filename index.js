0008988ea5465a12d7688fd902d7c8fa91fe2313
Michael Haileyesus <ayele@cs.helsinki.fi>
2018-09-06 13:40:32 +0300

    mongo data validation

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function createCourses() {
   });
 
   try {
-    await course.validate();
+    //await course.validate();
     // const result = await course.save();
     // console.log(result);
   } catch (error) {
